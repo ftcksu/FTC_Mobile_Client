ae5c2bd3f2a8f2faad61af3826fa5013d4853b99
o-aloqaily <osamaoqaily@gmail.com>
2019-01-14 20:06:26 +0300

    Added fonts

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Font } from 'expo'
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'Cairo-Bold': require('./assets/fonts/Cairo-Bold.ttf'),
+      'Cairo-SemiBold': require('./assets/fonts/Cairo-SemiBold.ttf'),
+      'Cairo-Light': require('./assets/fonts/Cairo-Light.ttf'),
+      'Cairo-Regular': require('./assets/fonts/Cairo-Regular.ttf'),
+      'Cairo-Black': require('./assets/fonts/Cairo-Black.ttf'),
+      'Cairo-ExtraLight': require('./assets/fonts/Cairo-ExtraLight.ttf'),
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      {
+        this.state.fontLoaded ?
+          <Text style={{ fontFamily: 'Cairo-Bold' }}>FTC your uncle</Text>
+        : null
+      }
       </View>
     );
   }
