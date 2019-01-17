633affb3cccfa5c2dbe8ec67a3f69aba9bd1b957
abo7atm <aaalnumay@gmail.com>
2019-01-17 21:32:51 +0300

    ....

--- src/reducers/AppReducer.js
+++ src/reducers/AppReducer.js
@@ -3,24 +3,16 @@ import { Font } from 'expo';
 
 const INITIAL_STATE = { fontHasLoaded: false };
 
-export default async function (state = INITIAL_STATE, action) {
+export default function (state = INITIAL_STATE, action) {
     // console.log(action.type)
     // console.log(FONT_LOADED===action.type)
     // console.log(action.type)
     switch (action.type) {
         case FONT_LOADED:
-            await Font.loadAsync({
-                'Cairo-Bold': require('../../assets/fonts/Cairo-Bold.ttf'),
-                'Cairo-SemiBold': require('../../assets/fonts/Cairo-SemiBold.ttf'),
-                'Cairo-Light': require('../../assets/fonts/Cairo-Light.ttf'),
-                'Cairo-Regular': require('../../assets/fonts/Cairo-Regular.ttf'),
-                'Cairo-Black': require('../../assets/fonts/Cairo-Black.ttf'),
-                'Cairo-ExtraLight': require('../../assets/fonts/Cairo-ExtraLight.ttf'),
-            });
             return {
                 fontHasLoaded: action.type,
             };
         default:
             return { state };
     }
-};
+}
