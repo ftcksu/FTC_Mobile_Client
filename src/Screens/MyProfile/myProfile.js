f1a7b6152f00f781803c1c22669cba9743c3b8f0
NAWIF <nnawif@gmail.com>
2019-01-23 03:49:24 +0300

    finished my profile screen, added charts and more

--- src/Screens/MyProfile/myProfile.js
+++ src/Screens/MyProfile/myProfile.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, ImageBackground } from 'react-native'
+import { View, StyleSheet, ImageBackground, Text } from 'react-native'
 import ScreenBackground from './screenBackground'
 import NameAndImage from './NameAndImage'
 import TotalPoints from './TotalPoints'
-import { Icon } from 'react-native-elements'
+import DoubleLineChart from './DoubleLineChart'
+import LineChart from "react-native-responsive-linechart";
 
+
+const data = [0, 40, 19, 32, 15, 52, 55, 20, 60, 78, 42, 56];
+// const config = {
+//   interpolation: "spline",
+//   line: { strokeColor: "green", strokeWidth: 2 },
+//   yAxis: { visible: false },
+//   area: {
+//     gradientFrom: "#999",
+//     gradientFromOpacity: 1,
+//     gradientTo: "#fff",
+//     gradientToOpacity: 1
+//   },
+//   grid: { visible: false 
+//   },
+//   backgroundColor: '#8888'
+// };
+const config = {
+  line: {
+    strokeWidth: 1,
+    strokeColor: "#c4f946"
+  },
+  area: {
+    gradientFrom: "#7281a7",
+    gradientFromOpacity: 1,
+    gradientTo: "#7281a7",
+    gradientToOpacity: 0,
+  },
+  yAxis: {
+    visible: false
+  },
+  grid: {
+    visible: false,
+    backgroundColor: "transparent",
+
+  },
+  interpolation: "none",
+  backgroundColor: "transparent",
+};
 export default class myProfile extends Component {
   render() {
     return (
@@ -12,7 +51,9 @@ export default class myProfile extends Component {
             
                 <NameAndImage/>
                 <TotalPoints/>
-                <View style={styles.chart} /> 
+                <View style={styles.chart} >
+                  <DoubleLineChart/>  
+                </View>
             </ImageBackground>      
     )
   }
@@ -27,9 +68,8 @@ const styles = StyleSheet.create({
        justifyContent:'space-evenly'
     },
     chart: {
-    //  marginTop:30,
-        height:'30%',
-        width:'100%',
-        backgroundColor:'blue'
-        }
+      height:'30%',
+      width:'90%',
+      // padding:15
+    }
   });
