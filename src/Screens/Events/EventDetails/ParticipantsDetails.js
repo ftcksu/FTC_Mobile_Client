10303b230cf06f30d31704ca2136cf5895b75e67
NAWIF <nnawif@gmail.com>
2019-02-08 16:17:25 +0300

    added whatsapp button, styles now are stored in style const

--- src/Screens/Events/EventDetails/ParticipantsDetails.js
+++ src/Screens/Events/EventDetails/ParticipantsDetails.js
@@ -22,12 +22,30 @@ export default class ParticipantsDetails extends Component {
         />
     )
   }
+  renderWhatsappButton(){
+    return (
+      <Button
+        icon={
+          <Icon
+            style={styles.buttonIcon}
+            name="whatsapp"
+            size={40}
+            color="white"
+          />
+        }
+        iconRight={true}
+        buttonStyle={styles.whatsappButton}
+        title="كلمه واتساب"
+        titleStyle={styles.whatsappButtonTitle}
+        />
+    )
+  }
   render() {
     
       console.log(this.props);
     return (
-      <TouchableOpacity onPress={this.props.callback} style={{height:'70%',width:'80%', backgroundColor:'white', alignSelf:'center', alignItems:'center'}} >
-        <Image style={{width:100, height: 100}} source={{uri:this.props.personDetails.image}} />
+      <TouchableOpacity onPress={this.props.callback} style={styles.container} >
+        <Image style={styles.image} source={{uri:this.props.personDetails.image}} />
         <Text style={styles.name} > {this.props.personDetails.first_name +" "+this.props.personDetails.last_name } </Text>
         {this.renderWhatsappButton()}
       </TouchableOpacity>
@@ -36,17 +54,37 @@ export default class ParticipantsDetails extends Component {
 }
 
 const styles={
+  container:{
+    justifyContent:'space-evenly',
+    height:'70%',
+    width:'80%', 
+    backgroundColor:'white', 
+    alignSelf:'center', 
+    alignItems:'center'
+  },
+  image:{
+    width: 150,
+    height: 150,
+    borderRadius: 150/2,
+  },
   name:{
-    fontFamily:'Cairo-Bold'
+    fontFamily:'Cairo-Bold',
+    fontSize:20
   },
   whatsappButton:{
     backgroundColor:'#2ecc71',
     height:75,
     width:'100%',
     marginTop:15,
+    alignContent:'center'
+    
 
   },
   whatsappButtonTitle:{
-    fontFamily:'Cairo-Bold'
+    fontFamily:'Cairo-Bold',
+    textAlign:'center'
+  },
+  buttonIcon:{
+    margin:10
   }
 }
