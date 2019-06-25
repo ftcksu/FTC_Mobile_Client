c476f0df6a031c23a281d7ad493d282740751db0
nawif <nnawif@gmail.com>
2019-06-25 10:52:45 +0300

    component now accept header and fixed data joining

--- src/components/shared_components/TasksTimeline/TasksTimeline.js
+++ src/components/shared_components/TasksTimeline/TasksTimeline.js
@@ -1,38 +1,54 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
-import { TasksMonthTimeline } from "../../";moment
-import * as lodash from "lodash";
-import * as moment from "moment";
+import { View, FlatList } from 'react-native'
+import { TasksMonthTimeline } from "../../";
+import * as _ from "lodash";
+	import moment from "moment";
 
-const dumbData = [
-  {
-      "description": "Hic ut sed ut debitis. Minus delectus rerum ratione vero iusto maiores dolorem voluptatem. Autem quod delectus cum necessitatibus temporibus nesciunt et.",
-      "date": "2019-06-17"
-  },
-  {
-      "description": "Asperiores reprehenderit facere qui porro. Sed id unde alias aut ea. Reprehenderit ratione debitis hic dolores. Praesentium est et repudiandae dolore voluptatum. Dolorem adipisci blanditiis eveniet quaerat perferendis.",
-      "date": "2019-06-17"
-  },
-  {
-      "description": "Et qui omnis sit provident quia perferendis ut. Et voluptatem quas reprehenderit quidem. Illo ipsum blanditiis quidem. Temporibus unde quo blanditiis quas similique omnis. Temporibus adipisci officia aut consequatur provident.",
-      "date": "2019-07-17"
-  },
-  {
-      "description": "Accusamus molestiae qui blanditiis nemo non iste error. Unde id ut sapiente exercitationem aut similique. Autem voluptas ex odio dolores aut corporis. Dolor nisi ipsam cum asperiores.",
-      "date": "2019-07-17"
-  }
-]
+console.disableYellowBox = true;
 export class TasksTimeline extends Component {
-  _monthName = item => moment(item.created_at, 'YYYY-MM-DD').format('MMM');
-  _getGroupedData = (arr) => lodash.groupBy(arr, this.monthName);
+
+  state={
+    tasks:[]
+  }
 
   componentDidMount(){
-    console.log(_getGroupedData(dumbData));
+    this.setState({
+      tasks:this.props.tasks
+    })
+  }
+  
+  _getGroupedData(arr){
+    return _.groupBy(arr, (item) => moment(item.date, 'YYYY-MM-DD').format('MMM'))
+  }
+
+  joinTasksByDate(tasks){
+    var groupedData = this._getGroupedData(tasks)
+    var monthsName = Object.keys(groupedData)
+    var tasks = Object.values(groupedData)
+    var joinedArrays = []
+    tasks.forEach((element, index) => {
+      obj = {
+        tasks:element,
+        date:monthsName[index]
+      }
+      joinedArrays.push(obj)
+    });
+    return(joinedArrays);
   }
   render() {
-
+    const data = this.joinTasksByDate(this.state.tasks);
     return (
-      <TasksMonthTimeline/>
+          <View >
+            <FlatList
+                ListHeaderComponent={this.props.header}
+                data={data}
+                renderItem={({ item, index }) => (
+                  <TasksMonthTimeline data ={item} isLast={(index+1 == data.length)} />
+                )}
+              />
+          </View>
+
     )
   }
 }
+
