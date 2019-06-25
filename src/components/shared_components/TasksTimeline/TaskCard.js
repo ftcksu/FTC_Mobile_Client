597aeefac1972011b6e0c3b71e45fa428a0cb28c
nawif <nnawif@gmail.com>
2019-06-25 10:50:11 +0300

    added bullet point and fix styling

--- src/components/shared_components/TasksTimeline/TaskCard.js
+++ src/components/shared_components/TasksTimeline/TaskCard.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
-import FTCStyledText from '../FTCStyledText'
-import { Icon } from 'react-native-elements/src/index';
+import {FTCStyledText} from '../../'
 
 export class TaskCard extends Component {
   render() {
     return (
       <View style={styles.container} >
-
-        <FTCStyledText style={styles.text} >{this.props.description}</FTCStyledText>
-
+        <FTCStyledText style={styles.text}>{this.props.description}</FTCStyledText>
+        <FTCStyledText style={styles.textBullet}>{'\u2B24'}</FTCStyledText> 
       </View>
     )
   }
@@ -18,19 +16,22 @@ export class TaskCard extends Component {
 const styles = StyleSheet.create({
     container: {
         width:"100%",
-       alignItems:'center',
        justifyContent:"space-evenly",
        flexDirection:'row',
     },
     text:{
         alignSelf:'center',
         fontFamily:'Cairo-Bold',
-        fontSize:18,
+        fontSize:13,
         color:'white',
-        // width:'50%'
-
+        flexDirection: 'column',
+        textAlign:'right',
+        flex: 0.9
     },
-    icon:{
-        // width:'50%'
+    textBullet:{
+      fontFamily:'Cairo-Bold',
+      fontSize:9,
+      marginTop:7,
+      color:'white',
     }
   });
