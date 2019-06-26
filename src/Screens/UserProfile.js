9587361435a9ef2a2fea88a007d9f3b094b25bce
nawif <nnawif@gmail.com>
2019-06-26 16:14:33 +0300

    replaced action button with social media list component

--- src/Screens/UserProfile.js
+++ src/Screens/UserProfile.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
-import { TasksTimeline, ScreenBackground, NameAndImage } from "../components";
-import ActionButton from 'react-native-circular-action-menu';
-import { FontAwesome } from '@expo/vector-icons';
-import * as _ from "lodash";
-import moment from "moment";
+import { View, StyleSheet, SafeAreaView, Image, Text } from 'react-native'
+import { TasksTimeline, ScreenBackground, NameAndImage, SocialMediaList, FTCStyledText } from "../components";
 import { Dimensions } from "react-native";
+import { FlatList } from 'react-native-gesture-handler';
+import imageAssets from "../../assets/images";
+import { FontAwesome } from '@expo/vector-icons';
 
 var width = Dimensions.get('window').width; //full width
-var height = Dimensions.get('window').height; //full height
-
-
-const SOCIALMEDIACIRCLESIZE = 45;
-const SOCIALMEDIAICONSIZE = 35
-
 
 export class UserProfile extends Component {
 
@@ -24,25 +17,28 @@ export class UserProfile extends Component {
 
     renderSocialMedia() {
         return(
-            <ActionButton
-                buttonColor="rgba(231,76,60,1)">
-                <ActionButton.Item buttonColor='#FFFC00' title="Snapchat" size={SOCIALMEDIACIRCLESIZE}>
-                    {/* <Image source={snapchat} style={{ height: 35, width: 35 }} tintColor={'white'} /> */}
-                    <FontAwesome name="snapchat" size={SOCIALMEDIACIRCLESIZE} />
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#0077B5' title="LinkedIn" size={SOCIALMEDIACIRCLESIZE}>
-                    <FontAwesome name="linkedin" size={SOCIALMEDIAICONSIZE} color="white" />
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#1DA1F2' title="Twitter" size={SOCIALMEDIACIRCLESIZE}>
-                    <FontAwesome name="twitter" size={SOCIALMEDIAICONSIZE} color="#F5F8FA" />
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#000000' title="Steam" size={SOCIALMEDIACIRCLESIZE}>
-                    <FontAwesome name="steam" size={SOCIALMEDIAICONSIZE} color="white" />
-                </ActionButton.Item>
-                <ActionButton.Item buttonColor='#25D366' title="Whatsapp" size={SOCIALMEDIACIRCLESIZE}>
-                    <FontAwesome name="whatsapp" size={SOCIALMEDIAICONSIZE} color="white" />
-                </ActionButton.Item>
-            </ActionButton>
+            // <ActionButton
+            //     buttonColor="rgba(231,76,60,1)">
+            //     <ActionButton.Item buttonColor='#FFFC00' title="Snapchat" size={SOCIALMEDIACIRCLESIZE}>
+            //         {/* <Image source={snapchat} style={{ height: 35, width: 35 }} tintColor={'white'} /> */}
+            //         <FontAwesome name="snapchat" size={SOCIALMEDIACIRCLESIZE} />
+            //     </ActionButton.Item>
+            //     <ActionButton.Item buttonColor='#0077B5' title="LinkedIn" size={SOCIALMEDIACIRCLESIZE}>
+            //         <FontAwesome name="linkedin" size={SOCIALMEDIAICONSIZE} color="white" />
+            //     </ActionButton.Item>
+            //     <ActionButton.Item buttonColor='#1DA1F2' title="Twitter" size={SOCIALMEDIACIRCLESIZE}>
+            //         <FontAwesome name="twitter" size={SOCIALMEDIAICONSIZE} color="#F5F8FA" />
+            //     </ActionButton.Item>
+            //     <ActionButton.Item buttonColor='#000000' title="Steam" size={SOCIALMEDIACIRCLESIZE}>
+            //         <FontAwesome name="steam" size={SOCIALMEDIAICONSIZE} color="white" />
+            //     </ActionButton.Item>
+            //     <ActionButton.Item buttonColor='#25D366' title="Whatsapp" size={SOCIALMEDIACIRCLESIZE}>
+            //         <FontAwesome name="whatsapp" size={SOCIALMEDIAICONSIZE} color="white" />
+            //     </ActionButton.Item>
+            // </ActionButton>
+            <View>
+                
+            </View>
         )
     }
     renderHeader = () => {
@@ -51,9 +47,9 @@ export class UserProfile extends Component {
                 <View style={styles.userInfoContainer}>
                     <NameAndImage src={ this.state.user.profilephoto_full_link } name={this.state.user.first_name +' '+ this.state.user.last_name} description={this.state.user.bio} style={styles.NameAndImage} imageStyle={styles.image} textStyle={styles.textStyle} showPulse={true}/>
                 </View>
-                <View style={styles.actionButton}>
-                    {this.renderSocialMedia()}
-                </View>
+                <SocialMediaList/>
+                <View style={styles.lineBreak} />
+                <FTCStyledText style={styles.title} >تاريخ أعمال {this.state.user.first_name}</FTCStyledText>
             </SafeAreaView>
         )
     }
@@ -70,11 +66,12 @@ export class UserProfile extends Component {
 
 const styles = StyleSheet.create({
     screenContainer:{
-        flex:1
+        flex:1,
     },
     headerContainer:{
         width: width - 16, // 16 is margin, width is full width
-        alignSelf:'center'
+        alignSelf:'center',
+        justifyContent:'center'
     },
     NameAndImage: {
         flex: 1,
@@ -93,9 +90,9 @@ const styles = StyleSheet.create({
         borderWidth: 2
     },
     actionButton: {
-        padding:60,
+        // padding:60,
         // flex: 1, 
-        // alignItems: 'center',
+        alignItems: 'center',
         // marginRight: 20,
         // marginTop: 50,
     },
@@ -103,6 +100,19 @@ const styles = StyleSheet.create({
       flexGrow: 0,
       paddingBottom:25,
       marginTop: 100,
-    }
-    
+    },
+    lineBreak: {
+        alignSelf: 'center',
+        width: '80%',
+        height: 5,
+        backgroundColor: '#eeeeee',
+        borderRadius:5,
+        marginBottom:16
+    },
+    title:{
+        fontFamily:'Cairo-Bold',
+        fontSize:20,
+        color:'white',
+        textAlign:'center'
+      }
   });
