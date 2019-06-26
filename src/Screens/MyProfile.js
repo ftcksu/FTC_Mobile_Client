81b079ff03ffc16ee27706ff005a9d4ecab576f9
nawif <nnawif@gmail.com>
2019-06-26 21:51:07 +0300

    fix myprofile styling

--- src/Screens/MyProfile.js
+++ src/Screens/MyProfile.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native'
 import { ScreenBackground, NameAndImage, TotalPoints, DoubleLineChart } from '../components'
 import { getLoggedInUserInfo } from '../global'
+import { ScrollView } from 'react-native-gesture-handler';
 
 export class MyProfile extends Component {
 
@@ -9,7 +10,8 @@ export class MyProfile extends Component {
     user :{
       first_name:'',
       last_name:'',
-      total_points:''
+      total_points:'',
+      profilephoto_full_link:''
     },
     tasks:{}
   }
@@ -20,34 +22,47 @@ export class MyProfile extends Component {
         user:response.data.user,
         tasks:response.data.tasks,
       })
-      console.log('getLoggedInUserInfo: ', response);
     }).catch(error => console.log('error: ' ,error))
   }
   
   onPress=()=>{
     this.props.navigation.navigate("History", {"tasks":this.state.tasks})
   }
+
+  renderNameAndImage(){
+    return(
+      <NameAndImage
+              imageStyle={styles.profilePhoto}
+              src={this.state.user.profilephoto_full_link}
+              name={this.state.user.first_name +' '+ this.state.user.last_name} 
+              titleStyle={styles.username} />
+    )
+  }
+
+  renderTotalPoints(){
+    return <TouchableOpacity style = {styles.totalPointsContainer} onPress={this.onPress} >
+            <TotalPoints points= {this.state.user.total_points} />
+    </TouchableOpacity>
+  } 
+
+  renderChart(){
+    return <View style={styles.chart} >
+      <DoubleLineChart/>  
+    </View>
+  }
   
   render() {
+    console.log(this.state.user);
     return (
       <View>
         <ScreenBackground/>
-        <View style={styles.container}  >
-
-          <NameAndImage src={this.state.user.profilephoto_full_link} name={this.state.user.first_name +' '+ this.state.user.last_name}/>
-
-          <TouchableOpacity onPress={this.onPress} >
-            <TotalPoints points= {this.state.user.total_points} />
-          </TouchableOpacity>
-
-          <View style={styles.chart} >
-              <DoubleLineChart/>  
-          </View>
-
-        </View>
-
-      </View>      
-    )
+          <SafeAreaView style={styles.container}  >
+            {this.renderNameAndImage()}
+            {this.renderTotalPoints()}
+            {this.renderChart()}
+            </SafeAreaView>
+        </View>      
+    ) 
   }
 }
 
@@ -56,11 +71,26 @@ const styles = StyleSheet.create({
        height:"100%",
        width:"100%",
        alignItems:'center',
-       justifyContent:'space-evenly',
+       justifyContent:'space-around',
 
     },
     chart: {
+      flex:1,
       height:'30%',
       width:'100%',
+    },
+    username:{
+      fontSize:20,
+      color: 'white'
+    },
+    profilePhoto: {
+      width: 150,
+      height:150,
+      borderRadius: 150/2 ,
+    },
+    totalPointsContainer:{
+      flex:1,
+      justifyContent:'center',
+
     }
   });
