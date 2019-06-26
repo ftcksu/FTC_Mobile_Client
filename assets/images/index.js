ffffbe5972a13fdfbc3809691731106fca599f4e
nawif <nnawif@gmail.com>
2019-06-26 16:13:24 +0300

    added social media icons and removed unused ones

--- assets/images/index.js
+++ assets/images/index.js
@@ -15,7 +15,16 @@ const images = {
 
     roundAdd: require('./round-add-button.png'),
     checkIcon: require('./check-icon.png'),
-    crownIcon: require('./crowns.png')
+    crownIcon: require('./crowns.png'),
+
+    //social media
+    whatsapp: require('./whatsapp.png'),
+    twitter: require('./twitter.png'),
+    steam: require('./steam.png'),
+    linkedin: require('./linkedin.png'),
+    snapchat: require('./snapchat.png'),
+
+
 
 }
 
