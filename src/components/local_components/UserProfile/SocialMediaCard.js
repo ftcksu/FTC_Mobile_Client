745c1e918dd9cc6f4b820ca3f17f9579f8471d13
nawif <nnawif@gmail.com>
2019-06-27 08:32:33 +0300

    clicking on social mdia icons now transfers you to it

--- src/components/local_components/UserProfile/SocialMediaCard.js
+++ src/components/local_components/UserProfile/SocialMediaCard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Image, View } from 'react-native'
+import { goToSocialMedia } from '../../../global'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 export class SocialMediaCard extends Component {
     render() {
         return (
-            <View>
+            <TouchableOpacity onPress={()=>goToSocialMedia(this.props.platform, this.props.username)}>
                 <Image style={[...this.props.iconStyle]} source = {this.props.icon} />
-            </View>
+            </TouchableOpacity>
         )
     }
 }
