cc028063d2e7e1cef9feb42d6a98924e7c2b570c
nawif <nnawif@gmail.com>
2019-07-04 16:04:25 +0300

    implemented auto compelete from scratch

--- src/components/local_components/AddEvent/AutocompleteEventParticipants.js
+++ src/components/local_components/AddEvent/AutocompleteEventParticipants.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import Autocomplete from 'react-native-autocomplete-input';
+import { Input } from 'react-native-elements'
 
 export class AutocompleteEventParticipants extends Component {
+  renderInput = () =>{
+    return (
+      <Input
+        placeholder={this.props.placeholder}
+        inputStyle={styles.inputStyle}
+        containerStyle={styles.containerStyle}
+        value={this.state.query}
+        onChangeText={text => this.setState({ query: text })}
+        inputContainerStyle={{borderBottomWidth: 0}}
+      />
+    )
+  }
 
   state = {
     query: ''
@@ -12,27 +25,32 @@ export class AutocompleteEventParticipants extends Component {
     const { query } = this.state;
     const filteredMembers = this.renderNames(query);
     return (
-      <Autocomplete style={styles.container}
-        containerStyle={{ marginTop: 15 }}
-        autoCorrect={false}
-        placeholder={'اكتب هنا المشاركين مبدئياً'}
-        data={filteredMembers}
-        defaultValue={query}
-        inputContainerStyle={{ borderWidth: 0 }}
-        onChangeText={text => this.setState({ query: text })}
-        renderItem={(item) => (
-          this.renderRow(item)
-        )}
-      />
+      <View containerStyle={styles.container} >
+        {this.renderInput()}
+        {filteredMembers.map(user => renderRow(user))}
+      </View>
+      // <Autocomplete
+      //   containerStyle={styles.container}
+      //   inputContainerStyle={{borderBottomWidth: 0}}
+      //   renderTextInput={ this.renderInput}
+      //   autoCorrect={false}
+      //   listContainerStyle={{height: 70 *  filteredMembers.length}}
+      //   data={filteredMembers}
+      //   defaultValue={query}
+      //   renderItem={(item) => (
+      //     this.renderRow(item)
+      //   )}
+      // />
     )
   }
 
   renderRow(item) {
+    this.props.scroll()
     return (
       <TouchableOpacity style={styles.autocompleteRow} onPress={() => this.onNamePress(item)} >
         <View style={styles.rowImageAndTextContainer} >
           <Text style={styles.textRow}>{item.first_name} {item.last_name}</Text>
-          <Image style={styles.imageRow} source={{ uri: item.image }} />
+          <Image style={styles.imageRow} source={{ uri: item.profilephoto_b64 }} />
         </View>
         <View style={styles.lineBreak} />
       </TouchableOpacity>
@@ -41,39 +59,39 @@ export class AutocompleteEventParticipants extends Component {
   }
 
   renderNames(query) {
+
     if (query === '') {
       return [];
     }
-
-    const { members } = this.props
-    const tmp = members.filter((member) => (`${member.first_name} ${member.last_name}`).includes(query))
-    if (tmp.length > 4) { // just to make sure that only a max of 4 items will be rendered.
-      return tmp.slice(0, 4)
+    const { users, enrolledUsers,  } = this.props
+    const enrolledUsersIDs = enrolledUsers.map(user => {
+      return user.id
+    }) 
+    const usersMatchQuery = users.filter((user) => (`${user.first_name} ${user.last_name}`).includes(query))
+    const usersMatchQueryAndNotAlreadyIn = usersMatchQuery.filter((user) => ! enrolledUsersIDs.includes(user.id))
+    if (usersMatchQueryAndNotAlreadyIn.length > 4) { // just to make sure that only a max of 4 items will be rendered.
+      return usersMatchQueryAndNotAlreadyIn.slice(0, 4)
     }
-    return tmp
+    return usersMatchQueryAndNotAlreadyIn
+
   }
 
-  onNamePress = (item) => {
+  onNamePress = (user) => {
     this.setState({ query: '' })
-    // updatestate(item) => add item to CurrentParticipants[], remove item from members[]
-    this.props.updateState(item)
+    this.props.onEnrollUser(user)
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    // padding:15,
-    textAlign: 'right',
-    paddingRight: 15,
-    height: 40,
-    fontSize: 14,
-    fontFamily: 'Cairo-Bold',
-    backgroundColor: "#eeeeee",
-    width: "105%",
-    alignSelf:'center'
+    flex:1,
+
+  },
+  inputContainerStyle:{
+    alignContent:'flex-end'
   },
   autocompleteRow: {
-    flexDirection: "column", alignItems: "flex-end", justifyContent: "flex-end",
+    flexDirection: "column", alignItems: "flex-end", justifyContent: "flex-end", height:70
   },
   rowImageAndTextContainer: {
     flexDirection: "row",
@@ -95,5 +113,14 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 2,
     backgroundColor: "#eeeeee"
-  }
+  },
+  inputStyle:{
+      textAlign: 'right',
+      fontFamily:'Cairo-Bold',
+      fontSize:13,
+      marginRight:8,
+  },
+  containerStyle:{
+    alignSelf: 'center',
+  },
 });
